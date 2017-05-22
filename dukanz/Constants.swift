012b63b3aeeb22cbd2af79526cd133fcb278ecf9
your Name <vokgoker@outlook.com>
2017-05-23 00:44:53 +0400

    Sign in ui custom classes somplete

---
+++ dukanz/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  dukanz
+//
+//  Created by mohamed alhammadi on 5/22/17.
+//  Copyright © 2017 alsenyardev. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
